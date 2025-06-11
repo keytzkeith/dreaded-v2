fc41ee70fc9f2b0e56c4b8e210daeaaeafeeab8f
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-06-11 18:32:10 +0300

    Update hidetag.js

--- Cmds/Groups/hidetag.js
+++ Cmds/Groups/hidetag.js
@@ -4,14 +4,13 @@ module.exports = async (context) => {
     await middleware(context, async () => {
         const { client, m, args, participants, text } = context;
 
-
-
-
-
-
-client.sendMessage(m.chat, { text : text ? text : '☞︎︎︎ TAGGED ☜︎︎︎' , mentions: participants.map(a => a.id)}, { quoted: m });
-
-});
-
+        await client.sendMessage(
+            m.chat,
+            { 
+                text: text ? text : '☞︎︎︎ TAGGED ☜︎︎︎', 
+                mentions: participants 
+            },
+            { quoted: m }
+        );
+    });
 }
-
