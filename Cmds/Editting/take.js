3f0b1b317ca03b0f9f1ed734f304706445fe678e
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-06-23 11:40:45 +0300

    Update take.js

--- Cmds/Editting/take.js
+++ Cmds/Editting/take.js
@@ -1,36 +1,38 @@
 const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
 
 module.exports = async (context) => {
+  const { client, m, pushname } = context;
 
-const { client, m, pushname, msgDreaded } = context;
+  const quotedMessage = m.msg?.contextInfo?.quotedMessage;
+  if (!quotedMessage) {
+    m.reply('Quote an image, a short video or a sticker to change watermark.');
+    return;
+  }
 
-if(!msgDreaded) { m.reply('Quote an image, a short video or a sticker to change watermark.') ; return } ;
-
-
-let media;
-if (msgDreaded.imageMessage) {
-     media = msgDreaded.imageMessage
-  } else if(msgDreaded.videoMessage) {
-media = msgDreaded.videoMessage
-  } 
-  else if (msgDreaded.stickerMessage) {
-    media = msgDreaded.stickerMessage ;
+  let media;
+  if (quotedMessage.imageMessage) {
+    media = quotedMessage.imageMessage;
+  } else if (quotedMessage.videoMessage) {
+    media = quotedMessage.videoMessage;
+  } else if (quotedMessage.stickerMessage) {
+    media = quotedMessage.stickerMessage;
   } else {
-    m.reply('This is neither a sticker, image nor a video...'); return
-  } ;
+    m.reply('This is neither a sticker, image nor a video...');
+    return;
+  }
 
-var result = await client.downloadAndSaveMediaMessage(media);
+  const result = await client.downloadAndSaveMediaMessage(media);
 
-let stickerResult = new Sticker(result, {
-            pack: pushname,
-            author: pushname,
-            type: StickerTypes.FULL,
-            categories: ["🤩", "🎉"],
-            id: "12345",
-            quality: 70,
-            background: "transparent",
-          });
-const Buffer = await stickerResult.toBuffer();
-          client.sendMessage(m.chat, { sticker: Buffer }, { quoted: m });
+  const stickerResult = new Sticker(result, {
+    pack: pushname,
+    author: pushname,
+    type: StickerTypes.FULL,
+    categories: ["🤩", "🎉"],
+    id: "12345",
+    quality: 70,
+    background: "transparent",
+  });
 
-}
+  const Buffer = await stickerResult.toBuffer();
+  client.sendMessage(m.chat, { sticker: Buffer }, { quoted: m });
+};
